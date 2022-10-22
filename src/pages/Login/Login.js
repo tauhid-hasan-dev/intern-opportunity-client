51d1d1ca4bb253893dc688afd3871e0ffc572913
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-22 18:18:22 +0300

    login page updated

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <p>this is login</p>
+        <div className='p-20  flex flex-col items-center'>
+            <form className="p-10 shadow-2xl w-[450px] " >
+                <p className='text-center text-3xl font-regular'>Login</p>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+
+                </div>
+                <div className='flex justify-between mt-3'>
+                    <div>
+                        <input type="checkbox" id="terms" name="terms" value="terms" />
+                        <label htmlFor="terms"> {<>
+                            Remember me
+                        </>}</label>
+                    </div>
+                    <div>
+                        <Link to='/terms' className="underline text-orange-500">Forgot Password?</Link>
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn bg-btn-color text-black border-btn-color hover:bg-orange-400 hover:border-orange-400 bg-orange-300 border-orange-300">Login</button>
+                </div>
+                <div className='text-center'>
+                    <small className='mr-2'>New to Intern Opportunity?</small>
+                    <Link to='/register' className="label-text-alt link link-hover text-orange-400">Create New Account</Link>
+                </div>
+            </form>
         </div>
     );
 };
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="navbar bg-slate-800  border-b border-slate-800 shadow-md px-5  lg:px-28 py-5 ">
+        <div className="navbar bg-slate-800  border-b border-slate-800 shadow-md px-5  lg:px-28 py-5">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden text-white text-6xl">
@@ -24,10 +24,12 @@ const Header = () => {
                         <NavLink to='/contact' className={({ isActive }) =>
                             isActive ? 'text-blue-color border-b-2 border-logo-color' : undefined
                         }>Contact</NavLink>
+                        <Link to='/login' className='lg:pl-8  pl-0'><button className="btn btn-outline btn-success">Login</button></Link>
                     </ul>
+
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-3xl  lg:text-4xl text-white font-bold ">
-                    tasan quiz app</Link>
+                    Intern Opportunity</Link>
             </div>
             <div className=" navbar-center hidden lg:flex h-16 w-16 ">
                 {/*  <FontAwesomeIcon icon={faBurger} /> */}
@@ -35,7 +37,7 @@ const Header = () => {
 
             </div>
             <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal p-0 text-slate-100 gap-10 text-lg ">
+                <ul className="menu menu-horizontal  text-slate-100 gap-10 text-lg ">
                     <NavLink
                         to="/home"
                         className={({ isActive }) =>
@@ -51,6 +53,7 @@ const Header = () => {
                         isActive ? 'text-blue-color border-b-2 border-logo-color' : undefined
                     }>Contact</NavLink>
                 </ul>
+                <Link to='/login' className='pl-8'><button className="btn btn- btn-success">Login</button></Link>
             </div>
         </div>
     );
