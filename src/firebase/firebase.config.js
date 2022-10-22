f1e59811d40a38bef9980dd1b0401c771ee47ead
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-22 17:01:47 +0300

    routes created, components are created, tailwind installed, daisy ui installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDU1XiwauyO18JF9mOSUfGEarMl4L-YRgc",
+    authDomain: "intern-opportunity-eecd0.firebaseapp.com",
+    projectId: "intern-opportunity-eecd0",
+    storageBucket: "intern-opportunity-eecd0.appspot.com",
+    messagingSenderId: "390741048037",
+    appId: "1:390741048037:web:b6d4fdc57e5d6cfb59f20c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/routes/routes/routes.js
@@ -0,0 +1,32 @@
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+import Main from "../../pages/Main/Main";
+import Register from "../../pages/Register/Register";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+
+        ]
+    }
+])
