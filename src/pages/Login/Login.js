b57fd2b64dbe238fa840ac9021f8c8a3dfbe460b
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-24 12:42:59 +0300

    modal added to the forgot password button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -101,9 +101,10 @@ const Login = () => {
                             Remember me
                         </>}</label>
                     </div>
-                    <div>
-                        <Link to='/terms' className="underline text-orange-500">Forgot Password?</Link>
+                    <div >
+                        <label htmlFor="my-modal-3" className="cursor underline text-orange-500">Forgot Password?</label>
                     </div>
+
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn bg-btn-color text-black border-btn-color hover:bg-orange-400 hover:border-orange-400 bg-orange-300 border-orange-300">Login</button>
@@ -119,6 +120,24 @@ const Login = () => {
             <div className='flex flex-col gap-3 mb-5 w-[25%]'>
                 <button onClick={handleGoogleSignIn} className="btn btn-outline rounded-full btn-warning flex gap-2"><FaGoogle /> Login with Google</button>
                 <button onClick={handleFacebookSignIn} className="btn btn-outline btn-primary rounded-full flex gap-2"><FaFacebook />Login with Facebook</button>
+            </div>
+
+            {/* Modal body */}
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal jus">
+                <div className="modal-box relative">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold">Please enter your email to reset password</h3>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control mt-6 flex-row justify-end">
+                        <button className="btn bg-btn-color text-black border-btn-color hover:bg-orange-400 hover:border-orange-400 bg-orange-300 border-orange-300">Update</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
